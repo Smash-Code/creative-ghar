f1a22152c66059e7dcb5426453f83a73d6c41eef
ashfaqFullstack <ashfaqfullstack@gmail.com>
2025-07-26 20:00:09 +0500

    project structure

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5-57IBYqDkXEuG4tQyvMrVK7bUfO3jRg",
+  authDomain: "creative-ghar.firebaseapp.com",
+  projectId: "creative-ghar",
+  storageBucket: "creative-ghar.firebasestorage.app",
+  messagingSenderId: "717579857649",
+  appId: "1:717579857649:web:581dfb083ae4b331de0350"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
