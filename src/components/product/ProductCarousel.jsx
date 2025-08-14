d3ab376c862437e398f5364bc7b83ba93f3da814
ashfaqFullstack <ashfaqfullstack@gmail.com>
2025-08-14 21:24:41 +0500

    reverse the carousel

--- src/components/product/ProductCarousel.jsx
+++ src/components/product/ProductCarousel.jsx
@@ -182,7 +182,7 @@ export default function ProductCarousel({ products }) {
                     ref={carouselRef}
                     className="flex transition-transform duration-500 ease-in-out"
                     style={{
-                        transform: `translateX(-${currentIndex * (120 / itemsToShow)}%)`,
+                        transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)`,
                         width: `${(totalItems / itemsToShow) * 100}%`
                     }}
                 >
